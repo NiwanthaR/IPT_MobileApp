49bd19b8eb56d3df75d15e3c2ff2d9f9ba8d0e7d
Niwantha Ranawaka <44175668+NiwanthaR@users.noreply.github.com>
2020-04-17 03:15:52 +0530

    Add Few UI

--- app/src/main/java/lk/demo/project/ipt_mobileapp/SplashScreenActivity.java
+++ app/src/main/java/lk/demo/project/ipt_mobileapp/SplashScreenActivity.java
@@ -12,11 +12,12 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(SplashScreenActivity.this,LoginActivity.class));
             }
-        },4000);
+        },2000);
     }
 }
