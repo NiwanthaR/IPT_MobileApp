200aa479695fd36f021904361e6f6b9489895755
Niwantha Ranawaka <44175668+NiwanthaR@users.noreply.github.com>
2020-04-18 22:02:06 +0530

    Update

--- app/src/main/java/lk/demo/project/ipt_mobileapp/MainActivity.java
+++ app/src/main/java/lk/demo/project/ipt_mobileapp/MainActivity.java
@@ -3,12 +3,26 @@ package lk.demo.project.ipt_mobileapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner language;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //read language
+        language = findViewById(R.id.test_drop);
+
+        ArrayAdapter<String> languageadapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.language_category));
+
+        languageadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        language.setAdapter(languageadapter);
     }
 }
